60dc6426e85c523281970e2832ed186db6faf8d3
Pat White <byelipk@gmail.com>
2019-02-23 08:00:59 -0500

    Add tracking

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,3 +1,11 @@
 module.exports = {
-  plugins: [`gatsby-plugin-typography`],
+  plugins: [
+    `gatsby-plugin-typography`,
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        trackingId: "UA-135083219-2"
+      }
+    }
+  ],
 }
