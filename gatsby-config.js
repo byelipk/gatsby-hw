4a5725c43d25c7b08cd0fbda043130147d46a0a2
Pat White <byelipk@gmail.com>
2019-02-23 08:15:54 -0500

    Upgrade to gtag

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,9 +2,10 @@ module.exports = {
   plugins: [
     `gatsby-plugin-typography`,
     {
-      resolve: `gatsby-plugin-google-analytics`,
+      resolve: `gatsby-plugin-gtag`,
       options: {
-        trackingId: "UA-135083219-2"
+        trackingId: "UA-135083219-2",
+        head: false
       }
     }
   ],
