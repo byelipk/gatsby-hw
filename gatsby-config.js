fa11df0d77dd18baf48d046b7552e07fe0d0113d
Pat White <byelipk@gmail.com>
2019-02-23 08:44:42 -0500

    go for it

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,12 +1,17 @@
 module.exports = {
   plugins: [
     `gatsby-plugin-typography`,
+    `gatsby-plugin-react-helmet`,
     {
       resolve: `gatsby-plugin-gtag`,
       options: {
-        trackingId: "UA-135083219-2",
-        head: false
-      }
+        // your google analytics tracking id
+        trackingId: `UA-135083219-2`,
+        // Puts tracking script in the head instead of the body
+        head: true,
+        // enable ip anonymization
+        anonymize: true,
+      },
     }
   ],
 }
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -2,10 +2,20 @@ import React from "react"
 import { Link } from "gatsby"
 import Layout from "../components/layout"
 
+import { Helmet } from "react-helmet"
+
 class Index extends React.Component {
   render() {
     return (
       <Layout>
+        <Helmet>
+          <title>Hahah Suckers</title>
+          <script>
+            {typeof window.gtag === "function"
+              ? window.gtag("config", "UA-135083219-2", { optimize_id: "GTM-PK5KLCL" })
+              : null}
+          </script>
+        </Helmet>
         <h1>Hi! I'm building a fake Gatsby site as part of a tutorial!</h1>
         <p>
           What do I like to do? Lots of course but definitely enjoy building
